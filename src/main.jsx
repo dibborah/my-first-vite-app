d969785a2c244729a0462526511e8bfb790fc59b
dibya.borah <dibya.borah@taazaa.com>
2024-03-30 16:43:52 +0530

    first commit

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import ReactDOM from "react-dom/client";
+
+function HelloWorld() {
+  return <h1>Hello Test</h1>
+}
+ 
+ReactDOM.createRoot(document.getElementById("root")).render(
+  <HelloWorld/>
+);
